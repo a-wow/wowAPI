ae1414fc7ddb1d68a864a1e09e4a6ef109ff8557
Null <107396951+a-wow@users.noreply.github.com>
2024-09-30 17:47:15 +0300

    Create config.php

---
+++ api/config.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$db = 'base_characters';
+$user = 'username';
+$pass = 'password';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database $db :" . $e->getMessage());
+}
+?>
